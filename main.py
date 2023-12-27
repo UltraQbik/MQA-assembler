e78a81796cb552d49b5320247d7070b8e3b271ed
Qubik <89706156+UltraQbik@users.noreply.github.com>
2023-12-27 03:30:23 +0300

    uky

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ def main():
     assembler.read_instruction_set()
     with open("test_program.mqa", "r") as file:
         print(
-            assembler.assemble_code(file.read())
-        )
+            assembler.assemble_code(file.read()))
 
 
 if __name__ == '__main__':
