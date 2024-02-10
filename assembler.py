edc2cf9e12db8b6a813ec540ce1d44441a919333
UltraQbik <no1skill@yandex.ru>
2024-02-10 20:46:31 +0300

    code spaghettification

--- assembler.py
+++ assembler.py
@@ -1,86 +1,102 @@
-def separate_tokens(code: str) -> list:
-    token = ""
-    token_list = []
+class Assembler:
+    def __init__(self):
+        self.code: str | None = None
+        self.token_list: list | None = None
 
-    char_ptr = 0
-    while char_ptr < len(code):
-        # get next character
-        char = code[char_ptr]
-        char_ptr += 1
+    def assemble(self, code: str):
+        """
+        Spaghetti code that assembles the code!
+        I know it's stupid, but I don't know how to make it better
+        :param code: Mini Quantum Assembly code
+        :return: none
+        """
 
-        if char == " " and token != "":
-            token_list.append(token)
-            token = ""
-        elif char in "\n," and token != "":
-            token_list.append(token)
-            token_list.append(char)
-            token = ""
-        elif char in "({[]})":
-            if token != "":
-                token_list.append(token)
+        self.code = code
+        self._separate_tokens()
+        self._delete_comments()
+        self.token_list = self._build_token_tree(self.token_list)
+
+    def _separate_tokens(self):
+        """
+        Separates the tokens
+        :return: none
+        """
+
+        token = ""
+        self.token_list = []
+
+        char_ptr = 0
+        while char_ptr < len(self.code):
+            # get next character
+            char = self.code[char_ptr]
+            char_ptr += 1
+
+            if char == " " and token != "":
+                self.token_list.append(token)
                 token = ""
-            token_list.append(char)
-        elif char != " " and char != "\n":
-            token += char
-    if token:
-        token_list.append(token)
-        token_list.append("\n")
-    return token_list
+            elif char in "\n," and token != "":
+                self.token_list.append(token)
+                self.token_list.append(char)
+                token = ""
+            elif char in "({[]})":
+                if token != "":
+                    self.token_list.append(token)
+                    token = ""
+                self.token_list.append(char)
+            elif char != " " and char != "\n":
+                token += char
+        if token:
+            self.token_list.append(token)
+            self.token_list.append("\n")
 
+    def _delete_comments(self):
+        """
+        Removes the comments to not clutter the token list
+        :return: none
+        """
 
-def delete_comments(token_list: list):
-    new_token_list = []
-    commented = False
-    for token in token_list:
-        if token == ";":
-            commented = True
-            continue
-        elif token == "\n":
-            commented = False
-        if not commented:
-            new_token_list.append(token)
-    return new_token_list
+        new_token_list = []
+        commented = False
+        for token in self.token_list:
+            if token == ";":
+                commented = True
+                continue
+            elif token == "\n":
+                commented = False
+            if not commented:
+                new_token_list.append(token)
+        self.token_list = new_token_list
 
+    def _build_token_tree(self, token_list: list):
+        """
+        Builds the token tree
+        :param token_list: yes
+        :return: none
+        """
 
-def build_token_tree(token_list: list):
-    tree = []
+        tree = []
+        index = 0
+        while index < len(token_list):
+            token = token_list[index]
+            index += 1
 
-    index = 0
-    while index < len(token_list):
-        token = token_list[index]
-        index += 1
+            if token in "({[":
+                scope = []
+                nesting = 0
+                while index < len(token_list):
+                    token = token_list[index]
+                    index += 1
 
-        if token in "({[":
-            bracket = token
-            opposite_bracket = {"(": ")", "{": "}", "[": "]"}[bracket]
+                    if token in "]})" and nesting == 0:
+                        tree.append(self._build_token_tree(scope))
+                        break
+                    elif token in "]})":
+                        nesting -= 1
+                    elif token in "({[":
+                        nesting += 1
 
-            scope = []
-            nesting = 0
-            while index < len(token_list):
-                token = token_list[index]
-                index += 1
-
-                if token == opposite_bracket and nesting == 0:
-                    tree.append(build_token_tree(scope))
-                    break
-                elif token == opposite_bracket:
-                    nesting -= 1
-                elif token == bracket:
-                    nesting += 1
-
-                scope.append(token)
+                    scope.append(token)
             else:
-                raise SyntaxError("no closing bracket")
-        else:
-            tree.append(token)
+                tree.append(token)
 
-    return tree
-
-
-def assemble_code(code: str):
-    tok_list = separate_tokens(code)
-    print(tok_list)
-    tok_list = delete_comments(tok_list)
-    print(tok_list)
-    tok_tree = build_token_tree(tok_list)
-    print(tok_tree)
+        return tree
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import assembler
+from assembler import Assembler
 
 
 def main():
+    asm = Assembler()
     with open("test_program.mqa", "r") as file:
-        print(
-            assembler.assemble_code(file.read())
-        )
+        asm.assemble(file.read())
+    print(asm.token_list)
 
 
 if __name__ == '__main__':
