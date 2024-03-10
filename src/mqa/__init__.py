6af0a20ef9db8453b204e09643c5bd3913f27037
UltraQbik <no1skill@yandex.ru>
2024-03-10 13:50:00 +0300

    Fix runtime warning
    Fixed warning:
    `RuntimeWarning: 'mqa.__main__' found in sys.modules after import of package 'mqa', but prior to execution of 'mqa.__main__'; this may result in unpredictable behaviour`

--- src/mqa/__init__.py
+++ src/mqa/__init__.py
@@ -1,6 +1,3 @@
-# I don't actually know how to use this,
-# but I hope that is how you do it
-
 from ._tokenizer import Tokenizer
 from ._compiler import Compiler
-from .__main__ import main
+from ._main import main
