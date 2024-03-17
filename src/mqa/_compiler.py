08eea3272a047263fddcfc92bc78ad9990068e27
UltraQbik <no1skill@yandex.ru>
2024-03-17 14:31:30 +0300

    New compile method placeholder

--- src/mqa/_compiler.py
+++ src/mqa/_compiler.py
@@ -8,4 +8,15 @@ class Compiler:
         The main compiler class
         """
 
+        self.main_scope: IScope = IScope([], BType.MISSING)
         self.main_scope_macros = {}
+
+    def compile(self, tree: TScope, main=True):
+        """
+        Main compile method for token scopes
+        :param tree: token tree
+        :param main: is the scope - main scope
+        :return: IScope
+        """
+
+        pass
